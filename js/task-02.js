031303cec61cb237dfc8cbc88fbd829480235cb0
Oleksandr Veremchuk <verem4uk@hotmail.com>
2022-07-10 22:28:03 +0300

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listIngredients = document.querySelector('#ingredients');
+// const listEl = ();
+ingredients.forEach(ingrediend => {
+  const itemEl = document.createElement('li');
+  itemEl.classList.add('item');
+  itemEl.textContent = ingrediend;
+  console.log(itemEl);
+  listIngredients.append(itemEl);
+  // listEl.push(itemEl);
+});
+// console.log(listEl);
+
